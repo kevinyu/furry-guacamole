0c3c30e1c9e122ee63072787d1bb68414a4871e6
Kevin Yu <thekevinyu@gmail.com>
2017-05-10 19:01:20 -0700

    Add a plotting func and also temporarily put that in lib... find better place for this code later

---
+++ lib/spikeplot/raster.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+
+def plot(spike_times, min_time=0.0, max_time=1.0):
+    """Raster plot based on spike arrival times
+   
+    Inputs:
+    spike_times (list (length N_units) of arrays of spike arrival times)
+    """
+
+    for i, spikes in enumerate(spike_times):
+        plt.vlines(spikes, i + 0.5, i + 1.5)
+
+    plt.vlines([0.0], 0.5, len(spike_times) + 0.5, color="r", linestyle="--")
+    plt.ylim(0.5, len(spike_times) + 0.5)
+    plt.xlim(min_time, max_time)
+    plt.yticks([])
+
